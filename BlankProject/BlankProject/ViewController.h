63b976502154f6f004d0793e15ec49bf9df814d8
Eddie <eddie.arpin@sportngin.com>
2014-08-06 11:52:36 -0500

    initial commit. adding project

---
+++ BlankProject/BlankProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlankProject
+//
+//  Created by Eddie Arpin on 8/6/14.
+//  Copyright (c) 2014 SportNgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
